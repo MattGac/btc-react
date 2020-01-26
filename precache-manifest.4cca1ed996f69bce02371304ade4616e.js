6c13f630110afdfe7af03ccd78999b8a745d26d7
Mateusz Gąciarz <schellers.box@gmail.com>
2020-01-26 16:47:49 +0100

    Updates

---
+++ precache-manifest.4cca1ed996f69bce02371304ade4616e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd88d46a9eb90185f0295daeb08c31c",
+    "url": "/btc-react/index.html"
+  },
+  {
+    "revision": "f60b3593b9413d212338",
+    "url": "/btc-react/static/css/main.974d284f.chunk.css"
+  },
+  {
+    "revision": "d53a0baae8b3db97334a",
+    "url": "/btc-react/static/js/2.a8f63be2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/btc-react/static/js/2.a8f63be2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f60b3593b9413d212338",
+    "url": "/btc-react/static/js/main.ee65729e.chunk.js"
+  },
+  {
+    "revision": "f5e4f9fe53aeba080c7b",
+    "url": "/btc-react/static/js/runtime-main.adf494f8.js"
+  }
+]);
